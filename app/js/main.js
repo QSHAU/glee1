f83b3cfd95c4e319bad87185ba4d7419016ad22f
QSHAU <lve1163@gmail.com>
2021-01-26 21:53:24 +0500

    added top slider, 2 mixitup, video popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
-$(function(){
+$(function () {
+
+    var containerEl1 = document.querySelector('[data-ref="mix1"]');
+    var containerEl2 = document.querySelector('[data-ref="mix2"]');
+
+    var mixer1 = mixitup(containerEl1, {
+    selectors: {
+      control: '.filter-1'
+    }
+  });
+
+    var mixer2 = mixitup(containerEl2, {
+      selectors: {
+        control: '.filter-2'
+      }
+  });
+
 
     $('.top-slider__inner').slick({
         dots: true,
@@ -8,4 +24,6 @@ $(function(){
         autoplaySpeed: 3000
     });
 
+
 });
+
